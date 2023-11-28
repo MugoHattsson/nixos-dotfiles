852d639f4b2fb15a37e9b108292da9fc9b11e278
MugoHattsson <hu5174ma-s@student.lu.se>
2023-11-28 12:06:49 +0100

    Switch bootloader to GRUB

--- configuration.nix
+++ configuration.nix
@@ -11,8 +11,14 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+
+    grub = {
+      efiSupport = true;
+      device = "nodev";
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.

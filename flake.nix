796fe11f5c5f73ada40885a84d72eff2ca80c5ac
MugoHattsson <hu5174ma-s@student.lu.se>
2023-12-01 19:36:00 +0100

    Upgraded to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -3,19 +3,27 @@
   description = "My first flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:  
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }:  
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      homeManagerModules = [ nixvim.homeManagerModules.nixvim ];
     in
   {
     nixosConfigurations = {
@@ -28,7 +36,7 @@
     homeConfigurations = {
       hugo = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [ ./home.nix ] ++ homeManagerModules;
       };
     };
   };

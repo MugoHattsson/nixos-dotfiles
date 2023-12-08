f0003ed0330efcbd9cece701ceb9ff1b6eb9086b
MugoHattsson <hu5174ma-s@student.lu.se>
2023-12-08 12:47:29 +0100

    Fix formatting

--- modules/gui/waybar.nix
+++ modules/gui/waybar.nix
@@ -7,39 +7,41 @@
 
       mainBar = {
         layer = "top";
-	position = "bottom";
-	height = 33;
+	    position = "bottom";
+	    height = 33;
 
-	modules-left = [ 
-	  "hyprland/workspaces" 
-	  "hyprland/language" 
-	  "hyprland/submap" 
-	];
-	modules-center = [ "hyprland/window" ];
-	modules-right = [ 
-	  "pulseaudio" 
-	  "network" 
-	  "battery" 
-	  "clock" 
-	  "tray"
-	];
+	    modules-left = [ 
+	      "hyprland/workspaces" 
+	      "hyprland/language" 
+	      "hyprland/submap" 
+	    ];
+	    modules-center = [ "hyprland/window" ];
+	    modules-right = [ 
+	      "pulseaudio" 
+	      "network" 
+	      "battery" 
+	      "clock" 
+	      "tray"
+	    ];
 
         "tray" = {
-          # "icon-size" = 21;
+           # "icon-size" = 21;
           "spacing" = 10;
         };
 
-	"clock" = {
-	  "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+	    "clock" = {
+	      "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
           "format-alt" = "{:%Y-%m-%d}";
-	};
+	    };
 
-	"battery" = {
-	  "states" = {
-	    "good" = 80;
-	    "warning" = 30;
-	    "critical" = 15;	    
-	  };
+        "battery" = {
+
+	      "states" = {
+	        "good" = 80;
+	        "warning" = 30;
+	        "critical" = 15;	    
+	      };
+
           "format" = "{capacity}% {icon}";
           "format-charging" = "{capacity}% ";
           "format-plugged" = "{capacity}% ";
@@ -47,22 +49,21 @@
           # "format-good" = ""; # An empty format will hide the module
           # "format-full" = "";
           "format-icons" = [ "" "" "" "" "" ];
-	};
 
-      
+	    };
+
+        
         "network" = {
             # "interface": "wlp2*"; # (Optional) To force the use of this interface
-            "format-wifi" = "{essid} ({signalStrength}%) ";
-            "format-ethernet" = "{ipaddr}/{cidr} ";
-            "tooltip-format" = "{ifname} via {gwaddr} ";
-            "format-linked" = "{ifname} (No IP) ";
-            "format-disconnected" = "Disconnected ⚠";
-            "format-alt" = "{ifname}: {ipaddr}/{cidr}";
+          "format-wifi" = "{essid} ({signalStrength}%) ";
+          "format-ethernet" = "{ipaddr}/{cidr} ";
+          "tooltip-format" = "{ifname} via {gwaddr} ";
+          "format-linked" = "{ifname} (No IP) ";
+          "format-disconnected" = "Disconnected ⚠";
+          "format-alt" = "{ifname}: {ipaddr}/{cidr}";
         };
 
-
       };
-
     };
   };
 }

8c76e128fcccf5cb85c146ef257d1a535b28c465
MugoHattsson <hu5174ma-s@student.lu.se>
2024-02-14 10:00:11 +0100

    Adjust formatting

--- modules/gui/waybar/waybar.nix
+++ modules/gui/waybar/waybar.nix
@@ -8,40 +8,40 @@
 
       mainBar = {
         layer = "top";
-	    position = "bottom";
-	    height = 33;
+        position = "bottom";
+        height = 33;
 
-	    modules-left = [ 
-	      "hyprland/workspaces" 
-	      "hyprland/language" 
-	      "hyprland/submap" 
-	    ];
-	    modules-center = [ "hyprland/window" ];
-	    modules-right = [ 
-	      "pulseaudio" 
-	      "network" 
-	      "battery" 
-	      "clock" 
-	      "tray"
-	    ];
+        modules-left = [ 
+          "hyprland/workspaces" 
+          "hyprland/language" 
+          "hyprland/submap" 
+        ];
+        modules-center = [ "hyprland/window" ];
+        modules-right = [ 
+          "pulseaudio" 
+          "network" 
+          "battery" 
+          "clock" 
+          "tray"
+        ];
 
         "tray" = {
-           # "icon-size" = 21;
+          # "icon-size" = 21;
           "spacing" = 10;
         };
 
-	    "clock" = {
-	      "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        "clock" = {
+          "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
           "format-alt" = "{:%Y-%m-%d}";
-	    };
+        };
 
         "battery" = {
 
-	      "states" = {
-	        "good" = 80;
-	        "warning" = 30;
-	        "critical" = 15;	    
-	      };
+          "states" = {
+            "good" = 80;
+            "warning" = 30;
+            "critical" = 15;	    
+          };
 
           "format" = "{capacity}% {icon}";
           "format-charging" = "{capacity}% ";
@@ -51,9 +51,9 @@
           # "format-full" = "";
           "format-icons" = [ "" "" "" "" "" ];
 
-	    };
+        };
 
-        
+
         "network" = {
           "format-wifi" = "{essid} ({signalStrength}%) ";
           "format-ethernet" = "{ipaddr}/{cidr} ";
